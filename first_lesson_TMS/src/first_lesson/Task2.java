bc4d08fb4de1ca53a2ac0f57f7950b5648e378bd
Vlad Zamkovets <mazvlad0808@gmail.com>
2021-11-10 22:11:30 +0300

    add first homework

---
+++ first_lesson_TMS/src/first_lesson/Task2.java
@@ -0,0 +1,12 @@
+package first_lesson;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int Var = 127;
+        byte bVar = (byte)Var;
+        System.out.println(bVar);
+        int secVar = 128;
+        byte sVar = (byte) secVar;
+        System.out.println(secVar);
+    }
+}
