b008672306ac5e4e3d0ae6fbe39100b42f9a5bf2
Vlad Zamkovets <mazvlad0808@gmail.com>
2021-11-11 14:41:48 +0300

    add second homework

--- first_lesson_TMS/src/first_lesson/Task10.java
+++
@@ -1,7 +0,0 @@
-package first_lesson;
-
-public class Task10 {
-    public static void main(String[] args) {
-        System.out.println("Символ 'a' встречается в строке \"Завтра будет хорошая погода!\" 4 раза");
-    }
-}
--- homework2/src/task10.java
+++ homework2/src/task10.java
@@ -1,2 +1,29 @@
-public class task10 {
-}
+
+    import java.util.Scanner;
+
+    public class task10 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int size = scanner.nextInt();
+            int[] arr = new int[size];
+            for (int i = 0; i < size; i++) {
+                arr[i] = scanner.nextInt();
+
+            }
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+
+            }
+            System.out.println();
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = i+1; j < arr.length; j++) {
+                    if (arr[i] == arr[j])
+                    {
+                        System.out.print(arr[i] + " ");
+                    }
+                }
+
+            }
+        }
+    }
+
